62a34e6ef9c8409843e01fe1df3170dbac6b0111
azwad <azwad_shameem@outlook.com>
2021-04-28 17:01:16 -0400

    Lab 2 - Completed - Grade Received: 100%

--- Lab 2/q1.c
+++ Lab 2/q1.c
@@ -4,25 +4,22 @@
 
 /* Write a program q1.c, and let the parent process produce two child processes.*/
 int main() {
-    pid_t pid1, pid2;
-    pid1 = fork();  // creates child 1 //
+    pid_t pid = fork();  // creates child 1 //
 
-    if (pid1 < 0) { // error checking in case fork does not create a child //
+    if (pid < 0) { // error checking in case fork does not create a child //
         perror("Forking error child 1");
         return -1;
-    } else if (pid1 == 0) {
-        printf("I am first child, my pid is: %d\n", getpid());  // the child 1 proccess prints out the desired string and its pid //
+    } else if (pid == 0) {
+        printf("I am first child, my pid is: %d\n", getpid());  // the child 1 process prints out the desired string and its pid //
     } else {
-        //wait(NULL); // waits for child 1 to terminate //
-        pid2 = fork();  // creates child 2 //           
-
-        if (pid2 < 0) { // error checking in case fork does not create a child //
+        wait(&pid); // waits for child 1 to terminate //
+        if ((pid = fork()) < 0) { // create child 2  and error checking in case fork does not create a child //
             perror("Forking Error with child 2");
             return -1;
         } else if (pid2 == 0) {
             printf("I am first second, my pid is: %d\n", getpid()); // child 2 prints out the desired string and its pid //
         } else {
-            wait(NULL); // waits for child 2 to terminate //
+            wait(&pid); // waits for child 2 to terminate //
             printf("Parent Process now Terminating.\n"); // prints parent terminating satement //
         }
     }
